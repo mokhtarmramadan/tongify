d71349eb91dbe443147ad7e6ead611ef714c491a
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-03-28 22:13:04 +0200

    Added the basemodel, some test cases and the file stoarge

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+''' BaseModel class that defines 
+all common attributes/methods for other classes'''
+import models
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    ''' The super class that all the other classes will inhurt from '''
+    def __init__(self, *args, **kwargs):
+        ''' intializes class attributes either using a dictionary passed to it (kwargs)
+        or by just taking an instance of the class '''
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    pass
+                elif k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+     
+    def save(self):
+        ''' Updates the public instance attribute updated_at
+        to the current time '''
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        '''Returns a dictionary containing all the keys/values of __dict__'''
+        dict_rep = self.__dict__.copy()
+        dict_rep["__class__"] = self.__class__.__name__
+        dict_rep["created_at"] = dict_rep["created_at"].isoformat()
+        dict_rep["updated_at"] = dict_rep["updated_at"].isoformat()
+        return dict_rep
+
+    def __str__(self):
+        ''' Returns a string representation of the class '''
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+
+    
---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+''' FileStorage '''
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    ''' serializes instances to a JSON file 
+    and deserializes JSON file to instances '''
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        ''' returns the dictionary __objects '''
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        ''' sets in __objects the obj with key <obj class name>.id '''
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        '''  serializes __objects to the JSON file (path: __file_path) '''
+        objsDictRep = {}
+        for key in FileStorage.__objects.keys():
+            objsDictRep[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(objsDictRep, f, indent=4)
+
+    def reload(self):
+        ''' deserializes the JSON file to __objects 
+        (only if the JSON file (__file_path) exists'''
+        try:
+            with open(FileStorage.__file_path) as f:
+                dict_objct = json.load(f)
+                for v in dict_objct.values():
+                    class_name = v["__class__"]
+                    del v["__class__"]
+                    self.new(eval(class_name)(**v))
+        except FileNotFoundError:
+            return
